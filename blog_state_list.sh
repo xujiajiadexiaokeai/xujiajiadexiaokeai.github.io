25bfc198ac4f5ead3fcfd90ed1d32a9b4ea02adf
Wenhao Jiang <whjiang1997@gmail.com>
2023-05-04 16:05:34 +0800

    Fix blog_state_list.sh

--- blog_state_list.sh
+++ blog_state_list.sh
@@ -5,8 +5,8 @@
 
 POSTS_DIR="content/posts"
 
-echo "\033[31mDrafts:\033[0m"
+echo -e "\033[31mDrafts:\033[0m"
 find $POSTS_DIR -type f -name "*.md" -exec grep -q "draft: true" {} \; -print | awk '{print "\033[31m"$0"\033[0m"}'
 
-echo "\033[32mPublished:\033[0m"
+echo -e "\033[32mPublished:\033[0m"
 find $POSTS_DIR -type f -name "*.md" -exec grep -q "draft: false" {} \; -print | awk '{print "\033[32m"$0"\033[0m"}'
